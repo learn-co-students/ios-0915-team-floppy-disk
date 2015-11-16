24eff6536332db4e58c4403dd3981c9c9de1a32f
kiaraRobles <kiara.robles@gmail.com>
2015-11-16 16:22:13 -0500

    Initialize repository with xcodeproj and .gitignore

---
+++ harpy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  harpy
+//
+//  Created by Kiara Robles on 11/16/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

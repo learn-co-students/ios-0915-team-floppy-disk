357091b76f542f210268de39be264170e8bca655
amyjoscelyn <amy.joscelyn@flatironschool.com>
2015-11-19 16:47:25 -0500

    added Google Place Picker but it thinks we're in Norway or something

--- harpy/HRPMapsViewController.m
+++ harpy/HRPMapsViewController.m
@@ -9,8 +9,8 @@
 #define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 
 #import "HRPMapsViewController.h"
+#import "HRPLocationManager.h"
 #import "HRPAddPostViewController.h"
-#import "CLLocationManager+Shared.h"
 #import <MapKit/MapKit.h>
 @import GoogleMaps;
 

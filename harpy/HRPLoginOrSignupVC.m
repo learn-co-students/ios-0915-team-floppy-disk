7a6b011f12daca13ce24ef14b8ae857ff9703b30
kiaraRobles <kiara.robles@gmail.com>
2015-11-18 19:04:40 -0500

    Progress on logins..

--- harpy/HRPLoginOrSignupVC.m
+++ harpy/HRPLoginOrSignupVC.m
@@ -12,6 +12,9 @@
 
 @interface HRPLoginOrSignupVC ()
 
+@property (nonatomic) UITextField *userNameNew;
+@property (nonatomic) UITextField *passwordNew;
+@property (nonatomic) UITextField *passwordConfirm;
 @property (nonatomic) UITextField *email;
 @property (nonatomic) UITextField *userName;
 @property (nonatomic) UITextField *password;
@@ -35,16 +38,18 @@
 }
 
 /*BUG: Does not allow input if constrants are applied on Storyboard*/
-
 - (IBAction)signUp:(id)sender {
     self.displayMessage.text = @"Sign up to find music curated locally";
     
+    self.email.hidden = NO;
+    self.userNameNew.hidden = NO;
+    self.passwordNew.hidden = NO;
+    self.passwordConfirm.hidden = NO;
+    
     self.userName.hidden = YES;
     self.password.hidden = YES;
-    self.email.hidden = NO;
-    
-    self.signup.hidden = NO;
     self.login.hidden = YES;
+    self.signup.hidden = NO;
     
     static dispatch_once_t onceToken; // Using dispatch_once to avoid multiple instances of the input fields
     dispatch_once(&onceToken, ^{
@@ -61,9 +66,44 @@
         self.email.delegate = self; // Required for dismissing the keyboard programically
         [self.inputView addSubview:self.email];
         
+        self.userNameNew = [[UITextField alloc] initWithFrame:CGRectMake(0, 40, 300, fieldHeight)];
+        self.userNameNew.placeholder = @"Username";
+        self.userNameNew.textAlignment = NSTextAlignmentCenter;
+        self.userNameNew.font = [UIFont fontWithName:@"helvetica-neue" size:14.0];
+        self.userNameNew.adjustsFontSizeToFitWidth = YES;
+        self.userNameNew.keyboardType = UIKeyboardTypeEmailAddress;
+        self.userNameNew.returnKeyType = UIReturnKeyDone;
+        self.userNameNew.returnKeyType = UIReturnKeyDefault;
+        self.userNameNew.delegate = self;
+        [self.inputView addSubview:self.userNameNew];
+        
+        self.passwordNew = [[UITextField alloc] initWithFrame:CGRectMake(0, 80, 300, fieldHeight)];
+        self.passwordNew.placeholder = @"Password";
+        self.passwordNew.textAlignment = NSTextAlignmentCenter;
+        self.passwordNew.font = [UIFont fontWithName:@"helvetica-neue" size:14.0];
+        self.passwordNew.adjustsFontSizeToFitWidth = YES;
+        self.passwordNew.keyboardType = UIKeyboardTypeEmailAddress; // Should change
+        self.passwordNew.returnKeyType = UIReturnKeyDone;
+        self.passwordNew.returnKeyType = UIReturnKeyDefault;
+        self.passwordNew.delegate = self;
+        self.passwordNew.secureTextEntry = YES;
+        [self.inputView addSubview:self.passwordNew];
+        
+        self.passwordConfirm = [[UITextField alloc] initWithFrame:CGRectMake(0, 120, 300, fieldHeight)];
+        self.passwordConfirm.placeholder = @"Password";
+        self.passwordConfirm.textAlignment = NSTextAlignmentCenter;
+        self.passwordConfirm.font = [UIFont fontWithName:@"helvetica-neue" size:14.0];
+        self.passwordConfirm.adjustsFontSizeToFitWidth = YES;
+        self.passwordConfirm.keyboardType = UIKeyboardTypeEmailAddress; // Should change
+        self.passwordConfirm.returnKeyType = UIReturnKeyDone;
+        self.passwordConfirm.returnKeyType = UIReturnKeyDefault;
+        self.passwordConfirm.delegate = self;
+        self.passwordConfirm.secureTextEntry = YES;
+        [self.inputView addSubview:self.passwordConfirm];
+        
         self.signup = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         [self.signup addTarget:self action:@selector(signupButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
-        [self.signup setFrame:CGRectMake(0, 80, 215, 40)];
+        [self.signup setFrame:CGRectMake(30, 160, 215, 40)];
         [self.signup setTitle:@"SignUp" forState:UIControlStateNormal];
         [self.signup setExclusiveTouch:YES];
         [self.inputView addSubview:self.signup];
@@ -73,11 +113,15 @@
     self.displayMessage.text = @"Log in to see music curated locally";
     
     self.email.hidden = YES;
+    self.userNameNew.hidden = YES;
+    self.passwordNew.hidden = YES;
+    self.passwordConfirm.hidden = YES;
+    
     self.userName.hidden = NO;
     self.password.hidden = NO;
     
-    self.signup.hidden = YES;
     self.login.hidden = NO;
+    self.signup.hidden = YES;
     
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -108,7 +152,7 @@
         
         self.login = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         [self.login addTarget:self action:@selector(loginButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
-        [self.login setFrame:CGRectMake(0, 80, 215, 40)];
+        [self.login setFrame:CGRectMake(30, 80, 215, 40)];
         [self.login setTitle:@"Login" forState:UIControlStateNormal];
         [self.login setExclusiveTouch:YES];
         [self.inputView addSubview:self.login];
@@ -117,7 +161,7 @@
 -(void) signupButtonClicked:(UIButton *)sender
 {
     NSLog(@"CLICKED: signup button");
-    [self.parseService createUserWithEmail:self.email.text completionHandler:^(HRPUser *user) {
+    [self.parseService createUser:self.userNameNew.text email:self.email.text password:self.passwordNew.text completionHandler:^(HRPUser *user) {
         
     }];
 }

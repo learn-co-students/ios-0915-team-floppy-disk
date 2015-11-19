ca3164fb4af69275b07eb354dbaf9ba71e4b442b
amyjoscelyn <amy.joscelyn@flatironschool.com>
2015-11-19 11:41:51 -0500

    added basic features to new view controller imports to class to get current location submit and cancel buttons with partial functionality

--- harpy/HRPAddPostViewController.m
+++ harpy/HRPAddPostViewController.m
@@ -7,27 +7,48 @@
 //
 
 #import "HRPAddPostViewController.h"
+//#import "HRPMapsViewController.h"
+#import "HRPLocationManager.h"
+#import "CLLocationManager+Shared.h"
+#import <MapKit/MapKit.h>
+@import GoogleMaps;
 
 @interface HRPAddPostViewController ()
+
+@property (nonatomic, strong) CLLocation *currentLocation;
+@property (nonatomic,strong) CLLocationManager *locationManager;
 
 @end
 
 @implementation HRPAddPostViewController
+{
+    GMSMapView *mapView_;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.currentLocation = self.locationManager.location;
 }
 
-//- (IBAction)addMarkerButtonTapped:(id)sender
-//{
-//    CLLocationCoordinate2D coordinate = [self.currentLocation coordinate];
-//    
-//    GMSMarker *marker = [[GMSMarker alloc] init];
-//    marker.icon = [GMSMarker markerImageWithColor:[UIColor blueColor]];
-//    marker.position = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude);
-//    marker.map = mapView_;
-//}
+- (IBAction)submitButtonTapped:(id)sender
+{
+        CLLocationCoordinate2D coordinate = [self.currentLocation coordinate];
+    
+        GMSMarker *marker = [[GMSMarker alloc] init];
+        marker.icon = [GMSMarker markerImageWithColor:[UIColor blueColor]];
+        marker.position = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude);
+        marker.map = mapView_;
+}
+
+- (IBAction)cancelButtonTapped:(id)sender
+{
+    
+}
+
+
+
 
 
 @end

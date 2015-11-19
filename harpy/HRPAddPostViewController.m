ce6cccd6925810cb98029f57b4b2dd297414ad65
amyjoscelyn <amy.joscelyn@flatironschool.com>
2015-11-19 10:54:58 -0500

    modal view controller added to map storyboard

---
+++ harpy/HRPAddPostViewController.m
@@ -0,0 +1,33 @@
+//
+//  HRPAddPostViewController.m
+//  harpy
+//
+//  Created by Amy Joscelyn on 11/19/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import "HRPAddPostViewController.h"
+
+@interface HRPAddPostViewController ()
+
+@end
+
+@implementation HRPAddPostViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+//- (IBAction)addMarkerButtonTapped:(id)sender
+//{
+//    CLLocationCoordinate2D coordinate = [self.currentLocation coordinate];
+//    
+//    GMSMarker *marker = [[GMSMarker alloc] init];
+//    marker.icon = [GMSMarker markerImageWithColor:[UIColor blueColor]];
+//    marker.position = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude);
+//    marker.map = mapView_;
+//}
+
+
+@end

b4cde188e539632c30e8846fccaff5cb212f21fd
milot31 <milot.phil@gmail.com>
2015-11-20 09:55:40 -0500

    still adding spotify authorization

--- harpy/HRPSpotifyViewController.m
+++ harpy/HRPSpotifyViewController.m
@@ -10,10 +10,10 @@
 #import <Spotify/Spotify.h>
 #import "HRPLoginRedirect.h"
 
-@interface HRPSpotifyViewController ()
+@interface HRPSpotifyViewController () <SPTAuthViewDelegate>
 
-@property (nonatomic, strong) SPTSession *session;
-@property (nonatomic, strong) SPTAudioStreamingController *player;
+@property (atomic, readwrite) SPTAuthViewController *authViewController;
+@property (atomic, readwrite) BOOL firstLoad;
 
 @end
 
@@ -22,18 +22,82 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [HRPLoginRedirect launchSpotify];
+    //[HRPLoginRedirect launchSpotify];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sessionUpdatedNotification:) name:@"sessionUpdated" object:nil];
+    self.firstLoad = YES;
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-}
-- (IBAction)play:(id)sender {
-    
 
+-(BOOL)prefersStatusBarHidden {
+    return YES;
 }
-- (IBAction)pause:(id)sender {
+
+//segues to player if notification center revceives an update on renewed session
+-(void)sessionUpdatedNotification:(NSNotification *)notification {
+    if (self.navigationController.topViewController == self) {
+        SPTAuth *auth = [SPTAuth defaultInstance];
+        if (auth.session && [auth.session isValid]) {
+            [self performSegueWithIdentifier:@"playerSegue" sender:nil];
+        }
+    }
 }
-- (IBAction)stop:(id)sender {
+
+//segue to player
+-(void)showSegue {
+    self.firstLoad = NO;
+    [self performSegueWithIdentifier:@"playerSegue" sender:nil];
+}
+
+-(void)authenticationViewController:(SPTAuthViewController *)authenticationViewController didFailToLogin:(NSError *)error {
+    
+}
+
+-(void)authenticationViewController:(SPTAuthViewController *)authenticationViewController didLoginWithSession:(SPTSession *)session {
+    //segue to initial app view
+}
+
+-(void)authenticationViewControllerDidCancelLogin:(SPTAuthViewController *)authenticationViewController {
+}
+
+-(void)openLogInPage {
+    
+    self.authViewController = [SPTAuthViewController authenticationViewController];
+    self.authViewController.delegate = self;
+    self.authViewController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    self.authViewController.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+    
+    self.modalPresentationStyle = UIModalPresentationCurrentContext;
+    self.definesPresentationContext = YES;
+    
+    [self presentViewController:self.authViewController animated:NO completion:nil];
+}
+
+-(void)renewTokenAndSegue {
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    
+    [auth renewSession:auth.session callback:^(NSError *error, SPTSession *session) {
+        auth.session = session;
+        
+        [self showSegue];
+    }];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    
+    if (auth.session == nil) {
+        
+    }
+    
+    if ([auth.session isValid] && self.firstLoad) {
+        [self showSegue];
+    }
+    
+    if (auth.hasTokenRefreshService) {
+        [self renewTokenAndSegue];
+    }
+}
+- (IBAction)logInClicked:(UIButton *)sender {
+    [self openLogInPage];
 }
 
 @end

8f909630966ce27e83b5b377203e207fd54ab769
kiaraRobles <kiara.robles@gmail.com>
2015-11-23 16:58:21 -0500

    Added profile class and view controller

---
+++ harpy/ProfileVC.m
@@ -0,0 +1,28 @@
+//
+//  ProfileVC.m
+//  harpy
+//
+//  Created by Kiara Robles on 11/23/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import "ProfileVC.h"
+
+@interface ProfileVC ()
+
+@end
+
+@implementation ProfileVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end

507bbf8585e9d48595b10cf18ca190ef4bac7aca
kiaraRobles <kiara.robles@gmail.com>
2015-11-24 10:29:13 -0500

    Connected logout button to parse backend

--- harpy/HRPSettingsVC.m
+++ harpy/HRPSettingsVC.m
@@ -7,21 +7,41 @@
 //
 
 #import "HRPSettingsVC.h"
+#import "HRPParseNetworkService.h"
 
 @interface HRPSettingsVC ()
+
+@property (strong, nonatomic) HRPParseNetworkService *parseService;
+
+- (IBAction)logout:(id)sender;
 
 @end
 
 @implementation HRPSettingsVC
 
+#pragma mark - Lifecycle methods
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.parseService = [HRPParseNetworkService sharedService];
 }
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Action methods
+
+- (IBAction)logout:(id)sender
+{
+    NSLog(@"CLICKED: logout button");
+    
+    {
+        [self.parseService logout];
+        [self performSegueWithIdentifier:@"sendToLogins" sender:sender];
+    }
 }
 
 @end

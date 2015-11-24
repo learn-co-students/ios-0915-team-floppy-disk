e4b2cd759142f9cc666f830716fa5baf558ab531
kiaraRobles <kiara.robles@gmail.com>
2015-11-24 16:09:23 -0500

    Added edit profile view

---
+++ harpy/HRPEditProfileVC.h
@@ -0,0 +1,13 @@
+//
+//  HRPEditProfileVC.h
+//  harpy
+//
+//  Created by Kiara Robles on 11/24/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRPEditProfileVC : UIViewController
+
+@end
---
+++ harpy/HRPEditProfileVC.m
@@ -0,0 +1,27 @@
+//
+//  HRPEditProfileVC.m
+//  harpy
+//
+//  Created by Kiara Robles on 11/24/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import "HRPEditProfileVC.h"
+
+@interface HRPEditProfileVC ()
+
+@end
+
+@implementation HRPEditProfileVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end

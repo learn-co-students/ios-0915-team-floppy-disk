1698e7fdde34a53859e71fd8ace7d82a11dbc177
kiaraRobles <kiara.robles@gmail.com>
2015-11-25 10:47:44 -0500

    Started development on user search

--- harpy/HRPMapsViewController.m
+++ harpy/HRPMapsViewController.m
@@ -10,11 +10,10 @@
 
 #import "HRPMapsViewController.h"
 #import "HRPLocationManager.h"
-#import "HRPAddPostViewController.h"
 #import <MapKit/MapKit.h>
 @import GoogleMaps;
 
-@interface HRPMapsViewController () <GMSMapViewDelegate, HRPAddPostViewControllerDelegate>
+@interface HRPMapsViewController () <GMSMapViewDelegate>
 
 @property (nonatomic, strong) GMSMapView *mapView;
 @property (weak, nonatomic) IBOutlet UIImageView *defaultMarkerImage;
@@ -208,29 +207,29 @@
 
 #pragma mark - Navigation
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    HRPAddPostViewController *addPostDVC = segue.destinationViewController;
-    addPostDVC.delegate = self;
-}
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+//{
+//    HRPAddPostViewController *addPostDVC = segue.destinationViewController;
+//    addPostDVC.delegate = self;
+//}
 
 #pragma mark - Modal View Controller
 
-// the methods below are for use with the modal view controller (+) button on top of the maps view.  it's all connected to the delegate and protocol of the HRPAddPostViewController class
-- (void)addPostViewController:(id)viewController didFinishWithLocation:(CLLocation *)location
-{
-    CLLocationCoordinate2D coordinate = [self.currentLocation coordinate];
-    
-    GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.icon = [GMSMarker markerImageWithColor:[UIColor blueColor]];
-    marker.position = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude);
-    marker.map = mapView_;
-    
-    [viewController dismissViewControllerAnimated:YES completion:nil];
-}
-- (void)addPostViewControllerDidCancel:(HRPAddPostViewController *)viewController
-{
-    [viewController dismissViewControllerAnimated:YES completion:nil];
-}
+//// the methods below are for use with the modal view controller (+) button on top of the maps view.  it's all connected to the delegate and protocol of the HRPAddPostViewController class
+//- (void)addPostViewController:(id)viewController didFinishWithLocation:(CLLocation *)location
+//{
+//    CLLocationCoordinate2D coordinate = [self.currentLocation coordinate];
+//    
+//    GMSMarker *marker = [[GMSMarker alloc] init];
+//    marker.icon = [GMSMarker markerImageWithColor:[UIColor blueColor]];
+//    marker.position = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude);
+//    marker.map = mapView_;
+//    
+//    [viewController dismissViewControllerAnimated:YES completion:nil];
+//}
+//- (void)addPostViewControllerDidCancel:(HRPAddPostViewController *)viewController
+//{
+//    [viewController dismissViewControllerAnimated:YES completion:nil];
+//}
 
 @end

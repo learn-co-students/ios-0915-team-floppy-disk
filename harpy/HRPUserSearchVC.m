bf11fcb14f446457b12ba8f5b1759e40f070d3d9
kiaraRobles <kiara.robles@gmail.com>
2015-11-27 17:13:08 -0500

    Added auto layout

--- harpy/HRPUserSearchVC.m
+++ harpy/HRPUserSearchVC.m
@@ -17,7 +17,6 @@
 @property (nonatomic) PFUser *user;
 @property (nonatomic) NSMutableArray *users;
 @property (strong, nonatomic) HRPParseNetworkService *parseService;
-@property (strong, nonatomic) NSMutableArray *userAndAvatars;
 
 @end
 
@@ -51,17 +50,18 @@
         }
     }];
 }
-
-- (void) viewWillAppear:(BOOL)animated {
+- (void) viewWillAppear:(BOOL)animated
+{
     [self.userTableView deselectRowAtIndexPath:[self.userTableView indexPathForSelectedRow] animated:animated];
     [super viewWillAppear:animated];
 }
 
--(void)initializeEmptyUsersArray {
-    self.users = [NSMutableArray new];
-    self.userAndAvatars = [NSMutableArray new];
-}
+#pragma mark - Setup methods
 
+-(void)initializeEmptyUsersArray
+{
+    self.users = [NSMutableArray new];
+}
 -(void)setupNavBar
 {
     [[UINavigationBar appearance] setTitleTextAttributes: @{
@@ -78,71 +78,16 @@
             completionBlock(objects, YES, error);
             NSLog(@"PFUser COUNT: %lu", (unsigned long)objects.count);
             self.users = [objects mutableCopy];
-            [self fetchAllUsersAvatars];
             [self.userTableView reloadData];
                 
-        } else {
+        }
+        else
+        {
             completionBlock(@[], NO, error);
-                
             NSLog(@"Error: %@ %@", error, [error userInfo]);
         }
     }];
-    
-    
 }
--(void)fetchAllUsersAvatars
-{
-    for (PFUser *user in self.users)
-    {
-        PFQuery *userQuery = [PFUser query];
-        [userQuery whereKey:@"objectId" equalTo:user.objectId];
-        [userQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-            if (!error)
-            {
-                NSLog(@"USER: %@", objects);
-                PFObject *user = [objects objectAtIndex:0];
-                PFFile *file = [user objectForKey:@"userAvatar"];
-                NSLog(@"FILE: %@", file);
-                
-                [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
-                 {
-                     if (!error)
-                     {
-                         UIImage *image = [UIImage imageWithData:data];
-                         NSLog(@"IMAGE: %@", image);
-                         [self.userAndAvatars addObject:image];
-                     }
-                 }];
-            }
-            else
-            {
-                NSLog(@"ERROR: %@ %@", error, [error userInfo]);
-            }
-        }];
-    }
-    NSLog(@"%@", self.userAndAvatars);
-}
-//
-//        PFFile *imageFile = [user objectForKey:@"userAvatar"];
-//        [imageFile getDataInBackgroundWithBlock:^(NSData *result, NSError *error) {
-//            if (!error)
-//            {
-//                UIImage *image = [UIImage imageWithData:result];
-//                [self.userAndAvatars setObject:image forKey:user.username];
-//            }
-//        }];
-//
-//- (void)getPhotoForUser:(PFUser *)user WithBlock:(void (^)(UIImage *photo))completionBlock
-//{
-//    PFFile *imageFile = [user objectForKey:@"userAvatar"];
-//    [imageFile getDataInBackgroundWithBlock:^(NSData *result, NSError *error) {
-//        if (!error)
-//        {
-//            UIImage *image = [UIImage imageWithData:result];
-//            completionBlock(image);
-//        }
-//    }];
-//}
 
 #pragma mark - Search bar methods
 
@@ -161,11 +106,10 @@
 {
     return self.users.count;
 }
-
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
     return 95.0;
 }
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [self.userTableView dequeueReusableCellWithIdentifier:@"userCell" forIndexPath:indexPath];
@@ -174,17 +118,15 @@
     UILabel *usernameLabel = (UILabel *)[cell viewWithTag:1];
     usernameLabel.text = user.username;
     
-    // UIImageView *userAvatar = (UIImageView *)[cell viewWithTag:2];
     cell.imageView.image = [UIImage imageNamed:@"spinner.png"];
-    
     PFFile *imageFile = [user objectForKey:@"userAvatar"];
-    //PFFile *imageFile = user[@"userAvatar"];
     [imageFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
-        if (!error) {
-            // userAvatar.image = [UIImage imageWithData:data];
+        if (!error)
+        {
             cell.imageView.image = [UIImage imageWithData:data];
         }
-        else {
+        else
+        {
             NSLog(@"ERROR: %@ %@", error, [error userInfo]);
         }
     }];

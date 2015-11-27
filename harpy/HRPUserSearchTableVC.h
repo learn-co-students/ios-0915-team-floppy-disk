11b30affb30975d12993fa8fbf9494b3f2589236
kiaraRobles <kiara.robles@gmail.com>
2015-11-27 16:55:49 -0500

    added user profile pic to search (not all in the right spot)

---
+++ harpy/HRPUserSearchTableVC.h
@@ -0,0 +1,14 @@
+//
+//  HRPUserSearchTableVC.h
+//  harpy
+//
+//  Created by Kiara Robles on 11/27/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+#import <Parse/Parse.h>
+
+@interface HRPUserSearchTableVC : PFQueryTableViewController
+
+@end

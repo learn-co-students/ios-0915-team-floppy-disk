b9c5d866b00b1a2b028bc69f7f914e0ce7b15b3b
milot31 <milot.phil@gmail.com>
2015-12-02 17:53:10 -0500

    getting canonical username

--- harpy/AppDelegate.m
+++ harpy/AppDelegate.m
@@ -61,9 +61,13 @@
     auth.redirectURL = [NSURL URLWithString:@"harpy-app://authorize"];
     auth.tokenSwapURL = [NSURL URLWithString:@"https://ios-0915-floppy-disk.herokuapp.com/swap"];
     auth.tokenRefreshURL = [NSURL URLWithString:@"https://ios-0915-floppy-disk.herokuapp.com/refresh"];
-    
+    NSLog(@"%@", auth.session.accessToken);
+    [SPTUser requestCurrentUserWithAccessToken:auth.session.accessToken callback:^(NSError *error, id object) {
+        NSLog(@"%@", object);
+    }];
     // Canonical username (currently Phils) needs to be saved so that different users can have persistent sessions
-    auth.sessionUserDefaultsKey = @"125204578";
+    
+    //auth.sessionUserDefaultsKey = @"125204578";
     
     
 //    for (NSString* family in [UIFont familyNames])
@@ -84,16 +88,21 @@
 {
     SPTAuth *auth = [SPTAuth defaultInstance];
     SPTAuthCallback authCallback = ^(NSError *error, SPTSession *session) {
-    // This is the callback that'll be triggered when auth is completed (or fails).
+        // This is the callback that'll be triggered when auth is completed (or fails).
+            
+        if (error != nil)
+        {
+            NSLog(@"*** Auth error: %@", error);
+            return;
+        }
         
-    if (error != nil)
-    {
-        NSLog(@"*** Auth error: %@", error);
-        return;
-    }
+        NSLog(@"Successful OAuth callback from Spotify. Access token: %@", session.accessToken);
+        [SPTUser requestCurrentUserWithAccessToken:session.accessToken callback:^(NSError *error, id object) {
+            NSLog(@"%@", object);
+        }];
         
-    auth.session = session;
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"sessionUpdated" object:self];
+        auth.session = session;
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"sessionUpdated" object:self];
     
     };
     

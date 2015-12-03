fe3f674a85990073c62b5045c1ab468f9fd9f834
milot31 <milot.phil@gmail.com>
2015-12-03 13:43:35 -0500

    adding container view

---
+++ harpy/Constants.m
@@ -0,0 +1,18 @@
+//
+//  Constants.m
+//  harpy
+//
+//  Created by Phil Milot on 12/3/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import "Constants.h"
+
+
+NSString * const LoggedInUserDefaultsKey = @"LoggenIn";
+
+NSString * const UserDidLogInNotificationName = @"UserDidLogInNotification";
+NSString * const UserDidLogOutNotificationName = @"UserDidLogOutNotification";
+
+NSString * const LogInViewControllerStoryboardID = @"LogInViewController";
+NSString * const HomeViewControllerStoryboardID = @"HomeViewController";

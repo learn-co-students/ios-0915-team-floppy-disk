b97e0374c42983576be84c363d279b5e85b4c8b8
kiaraRobles <kiara.robles@gmail.com>
2015-12-10 17:00:16 -0500

    Added offline alert

---
+++ harpy/HRPNetworkAlertVC.m
@@ -0,0 +1,33 @@
+//
+//  HRPNetworkAlertVC.m
+//  harpy
+//
+//  Created by Kiara Robles on 12/10/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import "HRPNetworkAlertVC.h"
+#import "HRPNetworkAlertView.h"
+
+@interface HRPNetworkAlertVC ()
+
+@property (nonatomic) HRPNetworkAlertView *alertView;
+
+@end
+
+@implementation HRPNetworkAlertVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor clearColor];
+    
+    self.alertView = [[HRPNetworkAlertView alloc] init];
+    [self.view addSubview:self.alertView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end

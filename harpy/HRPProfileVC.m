2de5d978415da91fe0e67a1bb1f56ed882a79dfd
milot31 <milot.phil@gmail.com>
2015-12-11 11:43:35 -0500

    posts are now sorted by date

--- harpy/HRPProfileVC.m
+++ harpy/HRPProfileVC.m
@@ -198,6 +198,7 @@
     [self.activityIndicator startAnimating];
     PFRelation *userPosts = [self.user relationForKey:@"HRPPosts"];
     PFQuery *query = [userPosts query];
+    [query orderByDescending:@"createdAt"];
     [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
         if (!error)
         {

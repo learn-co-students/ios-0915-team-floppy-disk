5b44fa1990615abce6651e48fad460a81013c85f
kiaraRobles <kiara.robles@gmail.com>
2015-12-12 15:09:16 -0500

    Autolayout on stack views

--- harpy/HRPMapsViewController.m
+++ harpy/HRPMapsViewController.m
@@ -94,7 +94,7 @@
     
     if (auth.session == nil) {
         NSLog(@"STATEMENT 3 TRUE");
-        [self openLogInPage];
+        //[self openLogInPage];
         [self.locationManager startUpdatingLocation];
         [self queryForHRPosts];
     }

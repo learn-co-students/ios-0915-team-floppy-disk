58b56d320d9db3622c106cb518740cc40272ad20
kiaraRobles <kiara.robles@gmail.com>
2015-12-12 21:12:44 -0500

    Started on queryable

---
+++ harpy/HYPProfileVC.h
@@ -0,0 +1,13 @@
+//
+//  HYPProfileVC.h
+//  harpy
+//
+//  Created by Kiara Robles on 12/12/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYPProfileVC : UIViewController
+
+@end
---
+++ harpy/HYPProfileVC.m
@@ -0,0 +1,37 @@
+//
+//  HYPProfileVC.m
+//  harpy
+//
+//  Created by Kiara Robles on 12/12/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import "HYPProfileVC.h"
+
+@interface HYPProfileVC ()
+
+@end
+
+@implementation HYPProfileVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
